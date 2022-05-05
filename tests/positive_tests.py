526527ca4a1aff4760ad03e73d21a0dd7f82c26b
DmitShu <dmitshu@gmail.com>
2022-05-05 19:56:13 +0300

    done

--- tests/positive_tests.py
+++ tests/positive_tests.py
@@ -7,7 +7,7 @@ class TestCalc:
         self.calc = Calculator
 
     def test_calculator_multiply_correctly(self):
-        assert self.calc.multiply(self, 2, 2) == 4
+        assert self.calc.multiply(self, 2, 5) == 10
 
     def test_calculator_division_correctly(self):
         assert self.calc.division(self, 10, 2) == 5
@@ -17,5 +17,3 @@ class TestCalc:
 
     def test_calculator_adding_correctly(self):
         assert self.calc.adding(self, 20000000000000, 1) == 20000000000001
-
-
