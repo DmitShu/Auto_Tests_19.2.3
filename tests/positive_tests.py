a534f274b3cec4f4c933c49bb94dc621d62a64a4
DmitShu <dmitshu@gmail.com>
2022-05-05 19:02:02 +0300

    calc test

--- tests/positive_tests.py
+++ tests/positive_tests.py
@@ -2,10 +2,11 @@ import pytest
 from app.calculator import Calculator
 
 class TestCalc:
+    # Позитивные тесты
     def setup(self):
         self.calc = Calculator
 
-    def test_multiply_calculate_correctly(self):
+    def test_multiply_correctly(self):
         assert self.calc.multiply(self, 2, 2) == 4
 
     def test_multiply_calculate_failed(self):
